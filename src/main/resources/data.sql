1d98931d752ea561c2f2588648c1584f1c017e5d
Guillem Plasencia <50019082+guillemplasencia@users.noreply.github.com>
2021-10-04 09:07:25 +0200

    Fixed data source to Oracle DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_details(email,firstname,lastname,password) VALUES ('admin@admin.com','admin','admin','admin');
+INSERT INTO user_details(email,firstname,lastname,password) VALUES ('john@gmail.com','john','doe','johndoe');
+INSERT INTO user_details(email,firstname,lastname,password) VALUES ('sham@yahoo.com','sham','tis','shamtis');
