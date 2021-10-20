b8886f3bc6c84c95c3352c929239e04ab5904ae9
liccioni <liccioni@gmail.com>
2021-10-20 09:31:52 +0200

    adding keycloak test conatinaer conected to oracle

--- src/test/java/com/example/springjpaoracle/TestContainersInitializer.java
+++ src/test/java/com/example/springjpaoracle/TestContainersInitializer.java
@@ -1,54 +1,60 @@
 package com.example.springjpaoracle;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.testcontainers.containers.BindMode;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.OracleContainer;
-import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.containers.*;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
+import java.time.Duration;
+
+@Slf4j
 public class TestContainersInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext>
 {
+
+    private final Network network = Network.newNetwork();
 
     private final RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3-management")
             .withExposedPorts(5672, 15672);
 
     private final OracleContainer oracle = new OracleContainer("oracle/database:18.4.0-xe")
+            .withNetwork(network)
+            .withNetworkAliases("oracle")
             .withFileSystemBind("oracle18.4.0XE", "/opt/oracle/oradata", BindMode.READ_WRITE)
-            .withExposedPorts(1521, 5500)
             .withUsername("testuser")
-            .withPassword("testpassword");
+            .withPassword("testpassword")
+            .withExposedPorts(1521, 5500);
 
     private final GenericContainer<?> keycloak = new GenericContainer<>(DockerImageName.parse("jboss/keycloak:15.0.2"))
-            //.withCommand("bin/standalone.sh -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/opt/jboss/keycloak/imports/realm-export.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING")
-            .withFileSystemBind("keycloak", "/opt/jboss/keycloak/imports")
+            .withNetwork(network)
+            .withFileSystemBind("keycloak/realm-export.json", "/opt/jboss/keycloak/imports/realm-export.json")
+            .withFileSystemBind("keycloak/ojdbc8.jar", "/opt/jboss/keycloak/modules/system/layers/base/com/oracle/jdbc/main/driver/ojdbc.jar")
             .withExposedPorts(8080)
+            .withEnv("JAVA_OPTS_APPEND", "-Dkeycloak.profile.feature.upload_scripts=enabled")
+            .withEnv("KEYCLOAK_IMPORT", "/opt/jboss/keycloak/imports/realm-export.json")
             .withEnv("KEYCLOAK_USER", "admin")
             .withEnv("KEYCLOAK_PASSWORD", "admin")
-            .withEnv("KEYCLOAK_IMPORT", "/opt/jboss/keycloak/imports/realm-export.json");
+            .withEnv("DB_VENDOR", "oracle")
+            .withEnv("DB_ADDR", "oracle")
+            .withEnv("DB_DATABASE", "XE")
+            .withEnv("DB_USER", "keycloak")
+            .withEnv("DB_PASSWORD", "keycloak")
+            .withEnv("DB_PORT", "1521")
+            .waitingFor(Wait.forHttp("/")
+                    .forStatusCode(200)
+                    .forStatusCode(301)
+                    .withStartupTimeout(Duration.ofMinutes(5)));
 
 
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext)
     {
-        startKeycloak(applicationContext);
-        startOracleDb(applicationContext);
         startRabbitMQ(applicationContext);
-    }
-
-    private void startKeycloak(final ConfigurableApplicationContext applicationContext)
-    {
-        keycloak.start();
-        final Integer mappedPort = keycloak.getMappedPort(8080);
-        final String host = keycloak.getHost();
-        final String certsUrl = String.format("http://%s:%d/auth/realms/springjpaoracle/protocol/openid-connect/certs", host, mappedPort);
-        final String issuerUrl = String.format("http://%s:%d/auth/realms/springjpaoracle", host, mappedPort);
-        TestPropertyValues.of(
-                "spring.security.oauth2.resourceserver.jwt.jwk-set-uri=" + certsUrl,
-                "spring.security.oauth2.resourceserver.jwt.issuer-uri=" + issuerUrl
-        ).applyTo(applicationContext.getEnvironment());
+        startOracleDb(applicationContext);
+        startKeycloak(applicationContext);
     }
 
     private void startRabbitMQ(ConfigurableApplicationContext applicationContext)
@@ -87,6 +93,20 @@ public class TestContainersInitializer implements ApplicationContextInitializer<
                 "spring.datasource.url=" + oracle.getJdbcUrl(),
                 "spring.datasource.username=" + oracle.getUsername(),
                 "spring.datasource.password=" + oracle.getPassword()
+        ).applyTo(applicationContext.getEnvironment());
+    }
+
+    private void startKeycloak(final ConfigurableApplicationContext applicationContext)
+    {
+
+        keycloak.start();
+        final Integer mappedPort = keycloak.getMappedPort(8080);
+        final String host = keycloak.getHost();
+        final String certsUrl = String.format("http://%s:%d/auth/realms/springjpaoracle/protocol/openid-connect/certs", host, mappedPort);
+        final String issuerUrl = String.format("http://%s:%d/auth/realms/springjpaoracle", host, mappedPort);
+        TestPropertyValues.of(
+                "spring.security.oauth2.resourceserver.jwt.jwk-set-uri=" + certsUrl,
+                "spring.security.oauth2.resourceserver.jwt.issuer-uri=" + issuerUrl
         ).applyTo(applicationContext.getEnvironment());
     }
 }
