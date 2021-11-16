aba7b815395ebd10805dc3fc9ec19a3aca4c811d
Leonardo Bernardes <4354753+leonardolemie@users.noreply.github.com>
2021-11-16 17:00:29 -0500

    Fix test assert using hibernate session factory

--- src/main/java/com/example/springjpaoracle/repository/CourseRepository.java
+++ src/main/java/com/example/springjpaoracle/repository/CourseRepository.java
@@ -3,9 +3,12 @@ package com.example.springjpaoracle.repository;
 import com.example.springjpaoracle.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Integer>
 {
     Optional<Course> findByNameIgnoreCase(String name);
+
+    List<Course> findByNameIgnoreCaseIn(List<String> name);
 }
